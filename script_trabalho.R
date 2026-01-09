6887a45aaa6d946c20256d7f6ea08b270943602f
lulidsn <luizanepomuceno@alu.ufc.br>
2026-01-09 14:12:27 -0300

    feat: adicionando testes de stepwise

--- script_trabalho.R
+++ script_trabalho.R
@@ -2,6 +2,8 @@
 
 rm(list = ls())
 
+#---------------*   *---------------
+
 #-------* Instalando e carregando pacotes necessários *-------
 
 pacotes <- c( "readr", "leaps", "MASS", "car", "ggplot2")
@@ -24,10 +26,45 @@ colnames(dados) <- c('x1', 'x2', 'x3', 'x4', 'x5', 'x6', 'x7', 'x8', 'x9', 'x10'
 
 dados <- dados[, c("y", setdiff(names(dados), "y"))] # alterando a posição de y, para ser a primeira coluna 
 
+#---------------* Análise Exploratória *---------------
+
 head(dados) 
 str(dados)       
 summary(dados)  
 pairs(dados)
 
+#---------------* Criação do modelo completo *---------------
+
 modelo_completo <- lm(y ~ ., data = dados)
 modelo_completo
+
+#---------------* Testando todas as possíveis regressões  *---------------
+
+subsets <- regsubsets(y ~ ., data = dados, nvmax = ncol(dados) - 1) # testa todas as combinações possíveis de regressão via critério <verificar qual o critério>
+summary(subsets) 
+
+summary(subsets)$adjr2 # com base no R2, o modelo com 8 variáveis é o 'melhor', com um R2 de 35.67% (ainda pode melhorar)
+summary(subsets)$cp # verificar
+summary(subsets)$bic # verificar (e checar se realmente entra nessa etapa)
+summary(subsets)$rss # verificar
+
+#---------------*  Métodos forward, backward e stepwise (TESTE)  *---------------
+
+modelo_forward <- step(modelo_completo, direction = 'forward')
+modelo_forward
+par(mfrow = c(2, 2))
+plot(modelo_forward)  
+
+modelo_backward <- step(modelo_completo, direction = 'backward')
+modelo_backward
+par(mfrow = c(2, 2))
+plot(modelo_backward)
+
+modelo_stepwise <- step(modelo_completo, direction = 'both')
+modelo_stepwise
+plot(modelo_stepwise)
+
+AIC(modelo_completo, modelo_forward, modelo_backward, modelo_stepwise)
+BIC(modelo_completo, modelo_forward, modelo_backward, modelo_stepwise)
+
+  
